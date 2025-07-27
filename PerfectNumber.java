61eb9aaf46125cb9f5bd7cbce95c6abf72585123
vineetha <vineetha.kudumu01@gmail.com>
2025-07-27 12:45:44 +0530

    Second commit

---
+++ PerfectNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to check if it's perfect: ");
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == num && num != 0) {
+            System.out.println(num + " is a Perfect Number.");
+        } else {
+            System.out.println(num + " is NOT a Perfect Number.");
+        }
+    }
+}
